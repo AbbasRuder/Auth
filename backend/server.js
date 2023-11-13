b79eeb8d8da5298412e20a798ce4dfd58ab2dbe6
Abbash Ali <abbasali6021@gmail.com>
2023-11-14 00:42:32 +0530

    Backend Initial setup🚀

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Working??')
+})
+
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}`)
+})
