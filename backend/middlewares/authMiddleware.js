1277881f2bc3200fabaee44176d88d8e0c4d1146
Abbash Ali <abbasali6021@gmail.com>
2023-11-17 18:12:39 +0530

    feat - add logout route and middleware to verify token

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken"
+import User from "../models/userModel.js"
+import asyncHandler from "express-async-handler"
+
+const authMiddleware = asyncHandler(async (req, res, next) => {
+    let token
+    token = req.cookies.jwt
+
+    if (token) {
+        try {
+            // -verifying the token using the secret 
+            const decode = jwt.verify(token, process.env.JWT_SECRET)
+
+            const user = await User.findById(decode.userId).select("-password")
+            req.user = user
+            next()
+        } catch (error) {
+            res.status(401)
+            throw new Error("User not authorized")
+        }
+    } else {
+        res.status(401)
+        throw new Error("Unauthorized without token")
+    }
+})
+
+export { authMiddleware }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,7 @@
 import express from 'express'
 const router = express.Router()
+// -middleware to verify token
+import { authMiddleware } from '../middlewares/authMiddleware.js'
 // -controllers
 import { 
     authUser, 
@@ -13,7 +15,7 @@ import {
 router.post('/auth', authUser)
 router.post('/', registerUser)
 router.post('/logout', logoutUser)
-router.get('/profile', getUserProfile)
-router.put('/profile', updateUserProfile)
+router.get('/profile', authMiddleware, getUserProfile)
+router.put('/profile', authMiddleware, updateUserProfile)
 
 export { router }
